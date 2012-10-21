72ce077a8e58bee8e13bf58471a5321c1c30fc01
dobbyzhang <dobbyzhang@gmail.com>
2012-10-21 18:57:06 -0400

    reinitialize for new design

--- version 2.0/JS/menu.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-	$('ul.menu li').hover(function(){
-		$(this).find('img').animate({left:'0px'},{queue:false,duration:250});
-	}, function(){
-		$(this).find('img').animate({left:'-140px'},{queue:false,duration:250});
-	});
-
-});
